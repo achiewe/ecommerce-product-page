be946d50d0dca1448b253d3ce27eff0b95babe8e
achiewe <mamukashviliachi706@gmail.com>
2023-05-29 18:38:24 +0400

    start project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import GlobalStyles from "./GlobalStyles";
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <GlobalStyles />
+    </div>
+  );
+}
+
+export default App;
