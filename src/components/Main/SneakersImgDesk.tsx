b1bbee0c398f27c4f4f97735a4edb86c37c8b760
achiewe <mamukashviliachi706@gmail.com>
2023-06-02 19:44:15 +0400

    add overlay div and make map method in sneakersimgdesk component

--- src/components/Main/SneakersImgDesk.tsx
+++ src/components/Main/SneakersImgDesk.tsx
@@ -1,17 +1,66 @@
 import styled from "styled-components";
+import { useState } from "react";
 import product1 from "../../images/image-product-1.jpg";
 import product2 from "../../images/image-product-2.jpg";
 import product3 from "../../images/image-product-3.jpg";
 import product4 from "../../images/image-product-4.jpg";
 import imgthumb1 from "../../images/image-product-1-thumbnail.jpg";
+import imgThumb2 from "../../images/image-product-2-thumbnail.jpg";
+import imgThumb3 from "../../images/image-product-3-thumbnail.jpg";
+import imgThumb4 from "../../images/image-product-4-thumbnail.jpg";
 
 const SneakersImgDesk = (): JSX.Element => {
-  const images = [product1, product2, product3, product4];
+  const imagesArray = [product1, product2, product3, product4];
+  const imagesThumb = [imgthumb1, imgThumb2, imgThumb3, imgThumb4];
+  const [chooseImage, setChooseImage] = useState<string>(imagesArray[0]);
+
+  const handleClick = (Img: string) => {
+    setChooseImage(Img);
+  };
+
   return (
     <MainDiv>
-      <img className="image-main" src={product1} alt="images" />
+      <img className="image-main" src={chooseImage} alt="images" />
       <div className="main-thumb">
-        <img className="image-thumb" src={imgthumb1} alt="image thumb" />
+        {imagesArray.map((image, index) => (
+          <img
+            key={index}
+            onClick={() => handleClick(image)}
+            className="image-thumb"
+            src={imagesThumb[index]}
+            alt="image thumb"
+          />
+        ))}
+      </div>
+      <div className="overlay-div">
+        <div className="overlay-imgmain">
+          <svg
+            className="close"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2,2 L18,18 M18,2 L2,18"
+              fill="none"
+              stroke="white"
+              strokeWidth="2"
+            />
+          </svg>
+          <img className="overlay-prod" src={product1} alt="image product" />
+        </div>
+        <div className="main-thumb">
+          {imagesArray.map((image, index) => (
+            <img
+              key={index}
+              onClick={() => handleClick(image)}
+              className="image-thumb"
+              src={imagesThumb[index]}
+              alt="image thumb"
+            />
+          ))}
+        </div>
       </div>
     </MainDiv>
   );
@@ -52,6 +101,38 @@ const MainDiv = styled.div`
         :hover {
           opacity: 0.5;
         }
+      }
+    }
+  }
+
+  .overlay-div {
+    width: 550px;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    gap: 40px;
+    justify-content: center;
+    background-color: black;
+    left: 32%;
+    top: 89px;
+    align-items: center;
+
+    .overlay-imgmain {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 24px;
+      justify-content: flex-end;
+      align-items: flex-end;
+
+      .close {
+        cursor: pointer;
+      }
+
+      .overlay-prod {
+        width: 550px;
+        height: 550px;
+        border-radius: 15px;
       }
     }
   }
